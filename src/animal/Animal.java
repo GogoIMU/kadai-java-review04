4246d5810d9ea293a657ea61298b5d31522508f1
GogoIMU <meiyou358@gmail.com>
2024-07-09 22:45:25 +0900

    2024/07/09課題作成

---
+++ src/animal/Animal.java
@@ -0,0 +1,33 @@
+package animal;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    // getterとsetter
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    // 引数なしのコンストラクタ
+    public Animal() {
+    }
+
+    // 引数ありのコンストラクタ
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+}
+    // sayメソッド
+    public void say() {
+        System.out.println(this.name + "です。" + this.age +"歳です。");
+    }
+}
